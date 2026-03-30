05f13b09edeb1ce39def73ddc9e10d26a262e2cd
Konrad Bauer <konrad.bauer@remoterebels.com>
2026-03-30 11:10:58 +0200

    docker config

--- init-ssl.sh
+++ init-ssl.sh
@@ -29,7 +29,8 @@ docker compose up -d nginx
 
 # Step 3: Request real certificate from Let's Encrypt
 echo ">>> Requesting Let's Encrypt certificate..."
-docker compose run --rm certbot certonly \
+docker compose run --rm --entrypoint "" certbot \
+  certbot certonly \
   --webroot \
   --webroot-path=/var/www/certbot \
   --email "$EMAIL" \
